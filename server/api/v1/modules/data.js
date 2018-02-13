bbe6dc99167116044833e10289b3db41b94349b0
Maxwell Carter <max4carter@gmail.com>
2018-02-13 14:55:34 -0500

    Fixed timestamp calculation

--- server/api/v1/modules/data.js
+++ server/api/v1/modules/data.js
@@ -7,20 +7,21 @@ module.exports = {
         log.info('Module [data] Function [save]')
         if (!req.auth.baby) {
             log.info('Internal Server Error: Token did not contain the required information.')
-            let err = new Error('Internal Server Error: Token did not contain the required information.')
+            let err = new Error('Internal Server Error: Could not identify baby.')
             err.status = 500
             next(err)
             return
         }
 
+        let timestamp = Date.now()
+
         if (req.body.heartrate && req.body.heartrate.length > 0) {
-            let timestamp = (req.body.timestamp) ? req.body.timestamp : Date.now()
             let collection = 'heartrate/' + req.auth.baby
             for (let i = 0; i < req.body.heartrate.length; i++) {
                 try {
                     let obj = {
                         bpm: req.body.heartrate[i],
-                        timestamp: timestamp + i * config.sampling.interval
+                        timestamp: timestamp + config.sampling.interval * (i - req.body.heartrate.length - 1)
                     }
                     ctrls.database.insert(collection, obj)
                 } catch (error) {
@@ -31,7 +32,6 @@ module.exports = {
         }
 
         if (req.body.movement && req.body.movement.length > 0) {
-            let timestamp = (req.body.timestamp) ? req.body.timestamp : Date.now()
             let collection = 'movement/' + req.auth.baby
             for (let i = 0; i < req.body.movement.length; i++) {
                 try {
@@ -39,7 +39,7 @@ module.exports = {
                         x: req.body.movement[i].x,
                         y: req.body.movement[i].y,
                         z: req.body.movement[i].z,
-                        timestamp: timestamp + i * config.sampling.interval
+                        timestamp: timestamp + config.sampling.interval * (i - req.body.heartrate.length - 1)
                     }
                     ctrls.database.insert(collection, obj)
                 } catch (error) {
@@ -50,13 +50,12 @@ module.exports = {
         }
 
         if (req.body.temperature && req.body.temperature.length > 0) {
-            let timestamp = (req.body.timestamp) ? req.body.timestamp : Date.now()
             let collection = 'temperature/' + req.auth.baby
             for (let i = 0; i < req.body.temperature.length; i++) {
                 try {
                     let obj = {
                         temperature: req.body.temperature[i],
-                        timestamp: timestamp + i * config.sampling.interval
+                        timestamp: timestamp + config.sampling.interval * (i - req.body.heartrate.length - 1)
                     }
                     ctrls.database.insert(collection, obj)
                 } catch (error) {
